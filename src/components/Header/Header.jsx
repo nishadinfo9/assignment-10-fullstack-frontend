db54b0eca87f02683cb6e76fb5c7039c2327f8c9
nishadinfo09 <mdnishadali03@gmail.com>
2025-11-16 17:14:00 +0600

    setup header and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,90 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Header = () => {
-  return <div>Header</div>;
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const navItem = [
+    { name: "Add Habits", path: "/add" },
+    { name: "My Habits", path: "/my-habits" },
+  ];
+
+  const logoutHandler = () => {};
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown"></div>
+        <Link to={"/"} className="btn btn-ghost text-xl">
+          Habit Tracker
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/all-habits"}>All Habits</Link>
+          </li>
+          {user &&
+            navItem.map((item, i) => (
+              <li key={i}>
+                <Link to={item.path}>{item.name}</Link>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          {user ? (
+            <>
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex="-1"
+                className="menu menu-md dropdown-content bg-base-100 rounded-box  shadow z-20"
+              >
+                <li>
+                  <p className="justify-between">{user?.displayName}</p>
+                </li>
+                <li>
+                  <p className="justify-between">{user?.email}</p>
+                </li>
+                <li>
+                  <button
+                    onClick={logoutHandler}
+                    className="justify-between bg-red-500 w-[70px] ml-3 text-center mt-1"
+                  >
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            </>
+          ) : (
+            <button
+              onClick={() => navigate("/login")}
+              className="btn btn-success"
+            >
+              Login
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
